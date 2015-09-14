9d182e823a8339af37e4c5e62e83ce0551cc37b1
Federico Iachetti <iachetti.federico@gmail.com>
2015-09-14 02:32:57 -0300

    Added some keybindings

--- fdx/settings/global-keybindings.el
+++ fdx/settings/global-keybindings.el
@@ -1,1 +1,10 @@
-(global-set-key (kbd "C-c g") 'magit-status)
+;; Magit
+(fdx/global-set-key (kbd "C-c g") 'magit-status)
+
+;; Yasnippet
+(fdx/global-set-key (kbd "<f7>") 'fdx/recompile-snippets)
+
+;; Text
+(fdx/global-set-key (kbd "M--") 'kill-whole-line)
+
+;; Multiple cursors
