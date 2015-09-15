4d528fc153ebcddfac639d81c4f07b43f34e0047
Federico Iachetti <iachetti.federico@gmail.com>
2015-09-15 19:05:42 -0300

    Added a function to unset and re-set a local keybinding

--- fdx/functions/set-keys.el
+++ fdx/functions/set-keys.el
@@ -3,3 +3,9 @@
   "Unset and re-set a keybinding"
   (global-unset-key key)
   (global-set-key key command))
+
+;;;###autoload
+(defun fdx/local-set-key (key command)
+  "Unset and re-set a keybinding"
+  (local-unset-key key)
+  (local-set-key key command))
