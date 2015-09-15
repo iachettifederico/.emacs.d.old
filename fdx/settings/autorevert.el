f4104ebcdf6f6ca5036b2916fb26842f61245f6d
Federico Iachetti <iachetti.federico@gmail.com>
2015-09-15 19:12:39 -0300

    Added some file convenience settings

---
+++ fdx/settings/autorevert.el
@@ -0,0 +1,6 @@
+;; Auto revert files when they change
+(global-auto-revert-mode t)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
