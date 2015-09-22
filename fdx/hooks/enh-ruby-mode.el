db4436becfa77e9690574632d5378bd815d79223
Federico Iachetti <iachetti.federico@gmail.com>
2015-09-22 09:04:38 -0300

    Added xmp filter goodies

--- fdx/hooks/enh-ruby-mode.el
+++ fdx/hooks/enh-ruby-mode.el
@@ -1,2 +1,20 @@
 (require 'ruby-electric)
 (electric-pair-mode t)
+
+(fdx/local-set-key (kbd "C-c C-c") 'xmp)
+(fdx/local-set-key (kbd "C-;") 'comment-dwim)
+
+;; ;; Compilation
+(fdx/local-set-key (kbd "C-\\") 'recompile)
+(fdx/local-set-key (kbd "M-\\") 'compile)
+
+(defadvice comment-dwim (around rct-hack activate)
+  "If comment-dwim is successively called, add => mark."
+  (if (and (eq major-mode 'enh-ruby-mode)
+           (eq last-command 'comment-dwim)
+           ;; TODO =>check
+           )
+      (insert "=>")
+    ad-do-it))
+
+(setq xmpfilter-command-name "ruby -S xmpfilter --no-warnings --dev --fork --detect-rbtest")
