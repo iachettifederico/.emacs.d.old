0651418299fef0c99a175257076ab776eb3dfa8d
Federico Iachetti <iachetti.federico@gmail.com>
2015-11-02 13:21:13 -0300

    Added indentation settings

---
+++ fdx/settings/indentation.el
@@ -0,0 +1,8 @@
+(setq-default indent-tabs-mode nil)
+(setq c-basic-offset 2)     ; indents 2 chars
+(setq tab-width 2)          ; and 2 char wide for TAB
+(setq indent-tabs-mode nil) ; And force use of spaces
+
+(setq css-indent-offset 2)  ; set CSS indentation to 2 spaces
+(setq js-indent-level 2)    ; set JavaScript indentation to 2 spaces
+(setq coffee-tab-width 2)   ; set CoffeeScript indentation to 2 spaces
