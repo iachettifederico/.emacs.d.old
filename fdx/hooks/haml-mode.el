9914f4bec4dfdc9e790cb6be1c57a84f67f9dc0e
Federico Iachetti <iachetti.federico@gmail.com>
2015-12-15 08:32:16 -0300

    Changed keybindings for erb2haml for haml mode

--- fdx/hooks/haml-mode.el
+++ fdx/hooks/haml-mode.el
@@ -1,2 +1,3 @@
 ;; Web programming
-(fdx/local-set-key (kbd "C-c C-e") 'fdx/erb-to-haml)
+(fdx/local-set-key (kbd "C-c C-e") 'fdx/erb-to-haml-current-buffer)
+(fdx/local-set-key (kbd "C-c e") 'fdx/erb-to-haml-on-region)
