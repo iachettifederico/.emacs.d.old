ce26ddd76cd7375432acce983f0fb4a67d69a9d3
Federico Iachetti <iachetti.federico@gmail.com>
2016-06-03 18:49:04 -0300

    Added indentation for TypeScript

--- fdx/settings/indentation.el
+++ fdx/settings/indentation.el
@@ -1,8 +1,10 @@
+(setq         indent-tabs-mode nil) ; And force use of spaces
 (setq-default indent-tabs-mode nil)
-(setq c-basic-offset 2)     ; indents 2 chars
-(setq tab-width 2)          ; and 2 char wide for TAB
-(setq indent-tabs-mode nil) ; And force use of spaces
 
-(setq css-indent-offset 2)  ; set CSS indentation to 2 spaces
-(setq js-indent-level 2)    ; set JavaScript indentation to 2 spaces
-(setq coffee-tab-width 2)   ; set CoffeeScript indentation to 2 spaces
+(setq c-basic-offset          2) ; indents 2 chars
+(setq tab-width               2) ; and 2 char wide for TAB
+
+(setq css-indent-offset       2) ; set CSS indentation to 2 spaces
+(setq js-indent-level         2) ; set JavaScript indentation to 2 spaces
+(setq coffee-tab-width        2) ; set CoffeeScript indentation to 2 spaces
+(setq typescript-indent-level 2) ; set TypeScript indentation to 2 spaces
