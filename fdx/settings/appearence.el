d05ef60d63a49ac2aead9c46b426eff9b2608d6c
Federico Iachetti <iachetti.federico@gmail.com>
2016-06-21 15:33:11 -0300

    Cleanded the appearence.el file

--- fdx/settings/appearence.el
+++ fdx/settings/appearence.el
@@ -1,22 +1,16 @@
-; (setq fdx/current-font "Inconsolata")
 (setq fdx/current-font-size 120)
 
 (custom-set-faces
  '(default ((t (:family "Inconsolata"
-			:foundry "unknown"
-			:slant normal
-			:weight normal
-			:height 120 :width normal)))))
+                        :foundry "unknown"
+                        :slant normal
+                        :weight normal
+                        :height 120
+                        :width normal)))))
 
 (show-paren-mode t)
 
 (require 'color-theme)
 (color-theme-initialize)
 
-;(require 'color-theme-ir-black)
-;(color-theme-sanityinc-tomorrow-night)
 (color-theme-sanityinc-tomorrow-bright)
-
-;(require 'ir-black-theme)
-
-;(color-theme-charcoal-black)
