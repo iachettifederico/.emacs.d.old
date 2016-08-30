ccc359696ae8ecad44d9c66bb1e2704dd34118b9
Federico Iachetti <iachetti.federico@gmail.com>
2016-08-30 11:47:17 -0300

    Configured line and column numbers

---
+++ fdx/settings/lines-and-columns.el
@@ -0,0 +1,15 @@
+;;;; Show line Numbers
+
+;; (global-linum-mode t)
+;; (set-face-attribute 'linum nil :background "#191919")
+
+(require 'nlinum)
+(global-nlinum-mode t)
+
+(setq nlinum-relative-redisplay-delay 0)        ;; delay
+;; (setq nlinum-relative-current-symbol "->")   ;; or "" for display current line number
+(setq nlinum-relative-offset 0)                 ;; 1 if you want 0, 2, 3...
+
+(column-number-mode)
+
+(global-hl-line-mode t)
