3fe4b911f8133c0428207165cc5f5833ddb9faf4
Federico Iachetti <iachetti.federico@gmail.com>
2016-08-30 11:41:08 -0300

    Added some org-mode settings

--- fdx/hooks/org-mode.el
+++ fdx/hooks/org-mode.el
@@ -1,4 +1,9 @@
-(fdx/local-set-key (kbd "C-c a") 'org-agenda)
+(fdx/local-set-key (kbd "C-c C-a") 'org-agenda)
+
+(setq truncate-lines nil)
+(setq truncate-partial-width-windows nil)
+
+
 (fdx/local-set-key (kbd "C-c t t") 'tapas/populate-tapas-shots)
 (fdx/local-set-key (kbd "C-c t r") 'tapas/tapas-shots)
 (fdx/local-set-key (kbd "C-c t e") 'tapas/insert-episode-link)
