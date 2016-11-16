9b103e681f51f77a3657e444959e814e04bca6c5
Federico Iachetti <iachetti.federico@gmail.com>
2016-11-16 15:04:43 -0300

    Uodated default configs

--- init.el
+++ init.el
@@ -1,9 +1,14 @@
 ;; FDX's Emacs configuration
 
 
-(package-initialize)
+;; (require 'autoload)
 
-(require 'autoload)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 (defun fdx/load-init-file (path &optional noerror)
   "This loads a file from inside the the .emacs.d directory"
