91e2e70ce8e0a3385c3c2a01c392b82e086b8423
Federico Iachetti <iachetti.federico@gmail.com>
2016-11-16 14:58:42 -0300

    Whitespace cleanup

--- fdx/hooks/org-mode.el
+++ fdx/hooks/org-mode.el
@@ -7,5 +7,3 @@
 (fdx/local-set-key (kbd "C-c t t") 'tapas/populate-tapas-shots)
 (fdx/local-set-key (kbd "C-c t r") 'tapas/tapas-shots)
 (fdx/local-set-key (kbd "C-c t e") 'tapas/insert-episode-link)
-
-
