46f021622fa132a98d9a643f904e5dcaddd0c607
Federico Iachetti <iachetti.federico@gmail.com>
2017-08-14 16:07:05 -0300

    Made compilation buffer better

--- fdx/settings/compilation-mode-colors.el
+++ fdx/settings/compilation-mode-colors.el
@@ -1,9 +1,9 @@
 (require 'ansi-color)
 (defun endless/colorize-compilation ()
-  "Colorize from `compilation-filter-start' to `point'."
+  "Colorize from 'compilation-filter-start' to 'point'."
   (let ((inhibit-read-only t))
     (ansi-color-apply-on-region
      compilation-filter-start (point))))
 
 (add-hook 'compilation-filter-hook
-          #'endless/colorize-compilation)
+          'endless/colorize-compilation)
