63a0238d9ccf033b7c27e57a6701c3c878fc06bd
Federico Iachetti <iachetti.federico@gmail.com>
2017-12-02 12:12:07 -0300

    Font increases and decreases in smaller increments

--- fdx/commands/fonts.el
+++ fdx/commands/fonts.el
@@ -2,12 +2,12 @@
 (defun fdx/increase-font-size (&optional by)
   "Increase Emacs font size"
   (interactive)
-  (setq fdx/current-font-size (+ fdx/current-font-size (or by 20)))
+  (setq fdx/current-font-size (+ fdx/current-font-size (or by 5)))
   (set-face-attribute 'default nil :height fdx/current-font-size))
 
 ;;;###autoload
 (defun fdx/decrease-font-size (&optional by)
   "Increase Emacs font size"
   (interactive)
-  (setq fdx/current-font-size (- fdx/current-font-size (or by 20)))
+  (setq fdx/current-font-size (- fdx/current-font-size (or by 5)))
   (set-face-attribute 'default nil :height fdx/current-font-size))
