a5214048de48e4880c65423f91929f32b4b1642a
Federico Iachetti <iachetti.federico@gmail.com>
2018-02-22 17:41:05 -0300

    Added command to make windows dedicated to their buffer

---
+++ fdx/commands/dedicated-window.el
@@ -0,0 +1,11 @@
+;;;###autoload
+(defun fdx/toggle-window-dedicated ()
+  "Control whether or not Emacs is allowed to display another
+buffer in current window."
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window (not (window-dedicated-p window))))
+       "%s: Can't touch this!"
+     "%s is up for grabs.")
+   (current-buffer)))
