aa79b63a759a8bb34d714e2be725dac3e6d9555c
Federico Iachetti <iachetti.federico@gmail.com>
2018-02-22 17:40:09 -0300

    Removed spureous function

--- fdx/commands/macros.el
+++ fdx/commands/macros.el
@@ -10,17 +10,3 @@
     (kmacro-name-last-macro (intern macro-name))
     (global-set-key (kbd key) (intern  macro-name))
     (message key)))
-
-;;;###autoload
-(defun fdx/kmacro-define-and-assign (char)
-  "Name last macro and assign a key combination"
-  (interactive "c")
-  (let (
-        (char-s (char-to-string char))
-        (macro-name (concat "fdx/last-macro-" (char-to-string char)))
-        (key (concat "H-" (char-to-string char)))
-        )
-    (kmacro-start-macro)
-    (kmacro-name-last-macro (intern macro-name))
-    (global-set-key (kbd key) (intern  macro-name))
-    (message key)))
