851bf9070032cd801dce129107ad360249dd9ada
Federico Iachetti <iachetti.federico@gmail.com>
2018-06-17 21:45:57 -0300

    Updated snippets.el

--- fdx/settings/snippets.el
+++ fdx/settings/snippets.el
@@ -8,21 +8,29 @@
 
 (defvar adhoc-snippet-last nil)
 
-(defun adhoc-snippet-create (begin end)
-  (interactive "r")
-  (let* ((text (concat
-                (buffer-substring-no-properties begin end)
-                ;; "$0" is required as an exit marker
-                "$0")))
-    (setq adhoc-snippet-last text)
-    ;; a lot of crazy things break if we don't turn this on
-    (yas-minor-mode 1)
-    (yas-expand-snippet text begin end)))
+;; Use ido for snippet prompt
+(setq yas-prompt-functions
+      (cons 'yas-ido-prompt
+            (remove 'yas-ido-prompt
+                    yas-prompt-functions)))
 
-(defun adhoc-snippet-insert ()
-  (interactive)
-  ;; a lot of crazy things break if we don't turn this on
-  (yas-minor-mode 1)
-  (yas-expand-snippet adhoc-snippet-last))
+;; (defun adhoc-snippet-create (begin end)
+;;   (interactive "r")
+;;   (let* ((text (concat
+;;                 (buffer-substring-no-properties begin end)
+;;                 ;; "$0" is required as an exit marker
+;;                 "$0")))
+;;     (setq adhoc-snippet-last text)
+;;     ;; a lot of crazy things break if we don't turn this on
+;;     (yas-minor-mode 1)
+;;     (yas-expand-snippet text begin end)))
+
+;; (defun adhoc-snippet-insert ()
+;;   (interactive)
+;;   ;; a lot of crazy things break if we don't turn this on
+;;   (yas-minor-mode 1)
+;;   (yas-expand-snippet adhoc-snippet-last))
+
+(setq yas-wrap-around-region t)  ; Automatically insert selected text at $0, if any
 
 (provide 'jasper-snippet)
